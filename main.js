a913720eb280207ffa6fb3010b545e71283f61cc
ASPSaraiva <156249201+ASPSaraiva@users.noreply.github.com>
2024-01-21 23:17:04 -0300

    Delete main.js

--- main.js
+++
@@ -1,9 +0,0 @@
-var ul = document.querySelector('nav ul');
-
-function toggleNavbar() {
-    ul.classList.toggle('open');
-}
-
-ul.addEventListener('click', function () {
-    ul.classList.remove('open');
-});
